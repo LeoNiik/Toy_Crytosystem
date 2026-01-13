a69d508635997eb15841b85cd6863968b3885994
LeoNiik <leonardonicoletta32@gmail.com>
2026-01-13 17:21:53 +0100

    Implemented arbitrary block size

---
+++ README.md
@@ -0,0 +1,4 @@
+### Toy Chipher
+
+This is a python (slow af) implemntation of an spn.
+The program takes a file path and a integer key ('-k' option) as input and writes the encrypted file on the path specified with the '-o' option.
--- SPN.py
+++ SPN.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def keySchedule(K, n, KEY_LEN_BITS):
     assert(KEY_LEN_BITS % 2 == 0)
@@ -15,11 +16,10 @@ def get_permutation_function(l, m):
     byteMap = {}
     arrPick = [i for i in range(l*m)]
 
-    byteShuffle = [i for i in range(16)]
+    byteShuffle = [i for i in range(pow(2,m))]
 
     random.shuffle(arrPick)
     random.shuffle(byteShuffle)
-    
     for i in range(len(arrPick)):
         posMap[i] = arrPick[i]   
     
@@ -38,133 +38,111 @@ class GoonChipher:
         self.nRounds = nRounds 
         self.masterKey = masterKey
         self.keys = keySchedule(masterKey, nRounds, l*m)
-        self.Pip, self.Pis = get_permutation_function(l, m) # Permutation
+        self.Pip, self.Pis = get_permutation_function(self.l, self.m) # Permutation
         self.invPis = {v: k for k, v in self.Pis.items()}
 
         return
-    
-
-    # Inputs an array l long of m bits integers and returns block long blockLen bytes object 
-    def pack(self, bitsArrr):
-        assert(len(bitsArrr) == self.l)
-        return bytes([(bitsArrr[i] << self.m) | bitsArrr[i+1] for i in range(0,len(bitsArrr),8//self.m)]) 
         
-    
-    # Input a block long blockLen returns an array l long of m bits 
-    def unpack(self, block):
-        bitsArr = []
+    def pack(self, bits, word_size=8):
+        assert len(bits) % word_size == 0
 
-        assert(len(block) > 0)
-        bitsInBlock = len(block) * 8
-        assert(bitsInBlock % self.m == 0)
+        out = []
+        for i in range(0, len(bits), word_size):
+            val = 0
+            for b in bits[i:i + word_size]:
+                val = (val << 1) | b
+            out.append(val)
 
+        return bytes(out)
         
-        byteCounter = 0
-        for byte in block:
-            tmpInt1 = ( byte & 0b11110000 ) >> 4
-            tmpInt2 = byte & 0b00001111
-
-            bitsArr.append(tmpInt1)
-            bitsArr.append(tmpInt2)
-        
-        
-        return bitsArr
-    
-
-    def S_box(self, block):
-        #unpack
-        byteArr = self.unpack(block)
-
-        byteArrCopy = byteArr.copy()
-
-        for i in range(len(byteArr)):
-            byteArr[i] = self.Pis[byteArrCopy[i]]
-        
-        return self.pack(byteArr) 
-    
-    def inverseS_box(self, block):
-        byteArr = self.unpack(block)
-        for i in range(len(byteArr)):
-            byteArr[i] = self.invPis[byteArr[i]]
-        return self.pack(byteArr)
-        
-
-    def inverseRoundPerm(self, block):
-        nibbles = self.unpack(block)
-        total_bits = len(nibbles) * self.m
-
-        # Flatten to bits
+    def unpack(self, data, word_size=8):
         bits = []
-        for nib in nibbles:
+        for byte in data:
+            for i in reversed(range(word_size)):
+                bits.append((byte >> i) & 1)
+        return bits
+
+    def bits_to_symbols(self, bits, m):
+        assert len(bits) % m == 0
+        symbols = []
+        for i in range(0, len(bits), m):
+            val = 0
+            for b in bits[i:i+m]:
+                val = (val << 1) | b
+            symbols.append(val)
+        return symbols
+    
+    def S_box(self, block):
+        bits = self.unpack(block)
+        symbols = self.bits_to_symbols(bits, self.m)
+        for i in range(len(symbols)):
+            symbols[i] = self.Pis[symbols[i]]
+
+        # symbols to bits
+        out_bits = []
+        for s in symbols:
             for i in reversed(range(self.m)):
-                bits.append((nib >> i) & 1)
+                out_bits.append((s >> i) & 1)
+
+        return self.pack(out_bits, 8)
+
+
+    def inverseS_box(self, block):
+        bits = self.unpack(block)
+        symbols = self.bits_to_symbols(bits, self.m)
+
+        for i in range(len(symbols)):
+            symbols[i] = self.invPis[symbols[i]]
+
+        # symbols to bits
+        out_bits = []
+        for s in symbols:
+            for i in reversed(range(self.m)):
+                out_bits.append((s >> i) & 1)
+
+        return self.pack(out_bits, 8)
+
+
+
+
+        
+    def inverseRoundPerm(self, block):
+        bits = self.unpack(block)
+        total_bits = self.l * self.m
 
         # Apply inverse permutation
         inv_bits = [0] * total_bits
         for i in range(total_bits):
             inv_bits[i] = bits[self.Pip[i]]
 
-        # Repack bits → nibbles
-        new_nibbles = []
-        for i in range(0, total_bits, self.m):
-            val = 0
-            for b in inv_bits[i:i+self.m]:
-                val = (val << 1) | b
-            new_nibbles.append(val)
-
-        return self.pack(new_nibbles)
-
-        
+        return self.pack(inv_bits)
 
     def roundPerm(self, block):
-        # Step 1: unpack into nibbles
-        nibbles = self.unpack(block)  # each element is m bits
-        total_bits = len(nibbles) * self.m
-
-        # Step 2: flatten to bit array
-        bits = []
-        for nib in nibbles:
-            for i in reversed(range(self.m)):
-                bits.append((nib >> i) & 1)
+        bits = self.unpack(block)  # each element is m bits
+        total_bits = self.l * self.m
 
         assert len(bits) == total_bits
 
-        # Step 3: apply permutation
         permuted_bits = [0] * total_bits
         for i in range(total_bits):
             permuted_bits[self.Pip[i]] = bits[i]
 
-        # Step 4: repack bits → nibbles
-        new_nibbles = []
-        for i in range(0, total_bits, self.m):
-            val = 0
-            for b in permuted_bits[i:i+self.m]:
-                val = (val << 1) | b
-            new_nibbles.append(val)
-
-        # Step 5: pack back to bytes
-        return self.pack(new_nibbles)
+        return self.pack(permuted_bits)
 
     def encryptBlock(self, block : bytes):
         assert(len(block) == self.blockLen)
-
         u = [bytes(0) for _ in range(self.nRounds)]
         v = [bytes(0) for _ in range(self.nRounds)]
         w = [bytes(0) for _ in range(self.nRounds)]
         
-        # u[0] = block ^ self.keys[0] 
         u[0] = bytes( a ^ b for a,b in zip(block,self.keys[0]))
         v[0] = self.S_box(u[0])
         w[0] = self.roundPerm(v[0])
-        # print(block , self.keys[0], u[0], v[0], w[0])
-
         
         for r in range(1,self.nRounds):
-            # u[r] = w[r-1] ^ self.keys[r]
             u[r] = bytes( a ^ b for a,b in zip(w[r-1],self.keys[r]))
             v[r] = self.S_box(u[r])
             w[r] = self.roundPerm(v[r])
-            # print(w[r-1], self.keys[r], u[r], v[r], w[r])
         
         return w[r]
     
@@ -172,21 +150,15 @@ class GoonChipher:
 
         assert(len(p) > 0)
         
-        c = b""
-        if len(p) % self.blockLen != 0:
-            for i in range(len(p)%self.blockLen):
-                p += b"\x41"
-
+        c = bytearray()
+        while(len(p) % self.blockLen != 0):
+            p += b"\x41"
+        
         # Divide p in blocks of m*l bits
-        blocks = []
-        block_count = 0
         for i in range(0,len(p),self.blockLen):
-            # print(f"====== block {block_count} ======")
-            tmpBlock = self.encryptBlock(p[i:i+self.blockLen])
-            c += tmpBlock
-            block_count += 1
+            c.extend(self.encryptBlock(p[i:i+self.blockLen]))
 
-        return c
+        return bytes(c)
     
     def decryptBlock(self, block):
         assert(len(block) == self.blockLen)
@@ -195,17 +167,11 @@ class GoonChipher:
         v = [bytes(0) for _ in range(self.nRounds)]
         w = [bytes(0) for _ in range(self.nRounds)]
         
-        # u[0] = block ^ self.keys[0] 
-        # print(block , self.keys[0], u[0], v[0], w[0])
-
         w[self.nRounds -1] = block
         for r in range(self.nRounds - 1, 0, -1):
-            # u[r] = w[r-1] ^ self.keys[r]
             v[r] = self.inverseRoundPerm(w[r])
             u[r] = self.inverseS_box(v[r])
             w[r - 1] = bytes( a ^ b for a,b in zip(u[r],self.keys[r]))
-            # print(v[r], u[r], self.keys[r], w[r - 1] , r)
-        
 
         v[0] = self.inverseRoundPerm(w[0])
         u[0] = self.inverseS_box(v[0])
@@ -216,49 +182,81 @@ class GoonChipher:
     
     def Decrypt(self, ciphertext):
         block = []
-        plaintext = b""
+        plaintext = bytearray()
 
         for i in range(0,len(ciphertext), self.blockLen):
-            tmpBlock = self.decryptBlock(ciphertext[i:i+self.blockLen])
-            plaintext += tmpBlock
+            plaintext.extend(self.decryptBlock(ciphertext[i:i+self.blockLen]))
         
-        return plaintext
+        return bytes(plaintext)
             
 
 
-# import argparse
+import argparse
 import sys
 
 
-# filePath = sys.argv[1]
+def main(args, chipher = None):
 
+    if (chipher == None):
+        chipher = GoonChipher(4,4,4,args.k)
 
-def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-d', help='description for option2')
-    
-    chipher = GoonChipher(4,4,4,1246)
-    
-    if len(sys.argv) < 2: exit(69)
-    
-    filePath = sys.argv[1]
-
+    filePath = args.filename
     file = open(filePath, 'rb')
     data = file.read()
-    
-    encryptedData = chipher.Encrypt(data)
-    outEncPath = f'{filePath}.enc' 
-    with open(outEncPath, 'wb') as f:
-        f.write(encryptedData)
-    
-    decData = chipher.Decrypt(encryptedData)
-    outDecPath = f'{filePath}.dec'
+    file.close()
 
-    with open(outDecPath, 'wb') as f:
-        f.write(decData)
-    
-    
-main()
+    if(not args.d):
+        print(f"Encrypting {filePath} -> {args.o}.enc")
+        encryptedData = chipher.Encrypt(data)
+        with open(f"{args.o}.enc", 'wb') as f:
+            f.write(encryptedData)
+            f.close()
+    else: 
+        print(f"Decrypting {filePath} -> {args.o}.dec")
+        decData = chipher.Decrypt(data)
+        with open(f"{args.o}.dec", 'wb') as f:
+            f.write(decData)
+            f.close()
 
+    return 0
+
+def benchmark(chipher,args):
+    start = time.time()
+    main(args, chipher)    
+    end = time.time()
+    print(f"[l={chipher.l}, m={chipher.m}, nr={chipher.nRounds} ]Elapsed time: {(end-start):.3f}s")
+    return
+
+def test(args):
+
+    l = 32
+    m = 16
+    nr = 4
+    chipher = GoonChipher(l,m,nr,1246)
+    benchmark(chipher, args)
+    
+    b = bytes([random.getrandbits(8) for _ in range((l*m)//8)])
+
+    assert chipher.pack(chipher.unpack(b), 8) == b
+    assert chipher.bits_to_symbols(chipher.unpack(b), 8) == list(b)
+
+    assert (chipher.inverseS_box(chipher.S_box(b)) == b)
+    assert (chipher.inverseRoundPerm(chipher.roundPerm(b)) == b)
+    
+    assert (chipher.Decrypt(chipher.Encrypt(b)) == b)
+    exit(0)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='Specify file path to encrypt/decrypt')
+    parser.add_argument('-k', required=True, default=1234,type=int, help= "Specify the key used to encrypt/decrypt")
+    parser.add_argument('-d', required=False,default=False, action='store_true', help='Specify decrypt mode')
+    parser.add_argument('-o', required=False,default="out", type=str, help='Specify output path')
+    args = parser.parse_args()
+
+    # test(args)
+    main(args)
+    exit(0)
 
 
